50283fe265013edd1583a09b48cb76570c87a916
Vassilis Papavassiliou <pav@geotagaeroview.com>
2015-12-09 18:44:39 +0200

    Added  setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='bottle-neck',
+    version='0.0.1beta',
+    packages=['bottle_neck'],
+    url='https://github.com/agile4you/bottle-neck.git',
+    license='GLPv3',
+    author='Papavassiliou Vassilis',
+    author_email='vpapavasil@gmail.com',
+    description='Web services with bottle.py made easy!',
+    extras_require={
+        'test': ['pytest'],
+    }
+)
